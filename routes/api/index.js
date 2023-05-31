a6c94f08f8f30cd9db414c4f2ef8e4c30ec3e35c
Jules <juliaghany@msn.com>
2023-05-31 14:25:13 -0500

    forgot to add require on line 3 and 4, fix that in this commit

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,7 @@
 // referring to Activity 28 -> routes -> api -> index.js
 const router = require('express').Router();
-const thoughtRoutes = ('./thoughtRoutes');
-const userRoutes = ('./userRoutes');
+const thoughtRoutes = require('./thoughtRoutes');
+const userRoutes = require('./userRoutes');
 
 router.use('/thoughts', thoughtRoutes);
 router.use('/users', userRoutes);
