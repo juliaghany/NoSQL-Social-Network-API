b49ab77869f25f2a7a34a392d5ee2d7bfbd66c51
Jules <juliaghany@msn.com>
2023-05-31 14:17:07 -0500

    add code that I need in order to deploy this application if necessary for this assignment, went through this with my tutor to gain experience with it

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 // referring to Activity 28 -> config -> connection.js
 const { connect, connection } = require('mongoose');
-
+// require("dotenv").config()
 const connectionString =
     process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialnetworkDB';
 
